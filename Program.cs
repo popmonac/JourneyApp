47a0e35efd060d67c516c16ca302ede2d8ae58b3
popmonac <78390497+popmonac@users.noreply.github.com>
2021-02-08 12:37:42 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+             try
+             {
+                 Console.WriteLine("Welcome");
+                 Console.WriteLine("Kindly enter Miles driven");
+                 double MileCovered = Convert.ToDouble(Console.ReadLine());
+                 Console.WriteLine("Kindly enter the gallon used");
+                 double GallonUsed = Convert.ToDouble(Console.ReadLine());
+
+                double MPG = (MileCovered / GallonUsed);
+
+                Console.WriteLine("Miles per Gallon = "+ MPG + " MPG ");
+
+                 Console.ReadLine();
+             }
+
+             catch (FormatException e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+
+             Console.ReadLine();
+
+             
+
+        }
+    }
+}
